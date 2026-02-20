06a5be5fa77f75fd1f7cc2abadc8bcb8b1a75071
oussama22kh <ossamathekh@gmail.com>
2026-02-21 00:44:37 +0100

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
+    <BrowserRouter basename={window.location.hostname.includes('github.io') ? '/student-foundation' : '/'}>      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
