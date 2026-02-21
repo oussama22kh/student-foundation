14a4581e7934a1e5879db5b4e1f0504bf2fcd56e
oussama22kh <ossamathekh@gmail.com>
2026-02-21 01:04:09 +0100

    Add scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import HomePage from './pages/HomePage'
 import ArchivePage from './pages/ArchivePage'
 import ThesisDetails from './pages/ThesisDetails'
@@ -9,6 +10,7 @@ import AdminDashboard from './pages/AdminDashboard'
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-academic-gray">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-grow">
         <Routes>
